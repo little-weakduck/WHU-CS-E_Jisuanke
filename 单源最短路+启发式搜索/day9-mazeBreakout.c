5ef5ae4c7add1b134c11ae84ad8177a2ba110212
Little Weak_duck <yinjiab@gmail.com>
2022-07-14 10:09:52 +0800

    a

--- 单源最短路+启发式搜索/day9-mazeBreakout.c
+++ 单源最短路+启发式搜索/day9-mazeBreakout.c
@@ -1,43 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
-#define maxn 5000
-#define maxm 101010
-
-const int inf = 0x3f3f3f3f;
-
-double min(double a, double b)
-{
-    if (a > b)
-    {
-        return b;
-    }
-    else
-    {
-        return a;
-    }
-}
-
-struct dot
+#define inf 1.0e+10
+int n, m;
+struct node
 {
     int x, y;
-} dots[201];
-
-double getLength(struct dot dot1, struct dot dot2)
-{
-    // printf("%d;%d;%d;%d;", dot1.x, dot1.y, dot2.x, dot2.y);
-    double length = sqrt((dot1.x - dot2.x) * (dot1.x - dot2.x) + (dot1.y - dot2.y) * (dot1.y - dot2.y));
-    // printf("%.2f", length);
-    return length;
-}
-
+} a[205];
 struct edge
 {
-    int v, next, f;
+    int v, next, f; // 1有效，0无效
     double w;
-} e[maxm * 2];
-int p[maxn], eid, n, m, s, vis[maxn];
-double d[maxn];
+} e[40005];
+int p[205], eid, s;
+int vis[205], pre[205];
+int path[205], cnt;
+double d[205], min;
 void insert(int u, int v, double w)
 {
     struct edge t = {v, p[u], 1, w};
@@ -49,27 +27,35 @@ void insert2(int u, int v, double w)
     insert(u, v, w);
     insert(v, u, w);
 }
-
-int main()
+double dist(int u, int v)
 {
-    scanf("%d%d", &n, &m); // s 是源点
-    memset(p, -1, sizeof(p));
-    for (int i = 0; i < n; i++)
+    double dx = a[u].x - a[v].x;
+    double dy = a[u].y - a[v].y;
+    return sqrt(dx * dx + dy * dy);
+}
+void getpath(int x)
+{
+    if (x == 1)
     {
-        scanf("%d%d", &dots[i ].x, &dots[i ].y);
+        path[cnt] = 1;
+        return;
     }
+    path[cnt++] = x;
+    getpath(pre[x]);
+}
 
-    for (int i = 0; i < m; i++)
-    {
-        int u, v;
-        scanf("%d%d", &u, &v);
-        insert2(u, v, getLength(dots[u - 1], dots[v - 1]));
-    }
-    memset(d, 0x3f, sizeof(d));
-    d[0] = 0;
+void dijkstra()
+{
     for (int i = 1; i <= n; i++)
     {
-        int mind = inf;
+        d[i] = inf;
+    }
+    memset(vis, 0, sizeof(vis));
+    s = 1;
+    d[s] = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        double mind = inf;
         int u = 0;
         for (int j = 1; j <= n; j++)
         {
@@ -80,19 +66,59 @@ int main()
             }
         }
         if (mind == inf)
-        {
             break;
-        }
         vis[u] = 1;
-        for (int j = p[u]; j != -1; j = e[j].next)
+        for (int j = p[u]; ~j; j = e[j].next)
         {
-            int v = e[j].v;
-            d[v] = min(d[v], d[u] + e[j].w);
+            if (e[j].f)
+            { //第一次找最短路时f均为1
+                int v = e[j].v;
+                if (d[v] > d[u] + e[j].w)
+                {
+                    d[v] = d[u] + e[j].w;
+                    pre[v] = u; //记录前一个点，只在第一次找最短路时有用处
+                }
+            }
         }
     }
+    if (d[n] < min)
+        min = d[n];
+}
+int main()
+{
+    scanf("%d%d", &n, &m);
     for (int i = 1; i <= n; i++)
     {
-        printf("%.2f ", d[i]);
+        scanf("%d%d", &a[i].x, &a[i].y);
     }
+    memset(p, -1, sizeof(p));
+    for (int i = 1; i <= m; i++)
+    {
+        int u, v;
+        scanf("%d%d", &u, &v);
+        insert2(u, v, dist(u, v));
+    }
+    dijkstra();
+    getpath(n);
+    min = inf;
+    for (int i = 0; i < cnt; i++)
+    {
+        int u1 = path[i], v1 = path[i + 1];
+        for (int j = p[u1]; ~j; j = e[j].next)
+        {
+            if (e[j].v == v1)
+            {
+                e[j].f = 0;     //删除最短路径上的一条边
+                e[j ^ 1].f = 0; //无向图两条边序号相邻，用抑或处理
+                dijkstra();
+                e[j].f = 1;
+                e[j ^ 1].f = 1;
+            }
+        }
+    }
+    if (min == inf)
+        printf("-1");
+    else
+        printf("%.2f", min);
     return 0;
 }
